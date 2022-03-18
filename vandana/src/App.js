ae352cf3aa1aa124551177b97604a85e607ee8c0
dileep <valluridileep999@gmail.com>
2022-03-18 11:55:35 +0530

    first

---
+++ vandana/src/App.js
@@ -0,0 +1,20 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     <h1>Hello Visakha</h1>
+     <ul>
+       <li><a href="Home">Home</a></li>
+       <li><a href='about'>about</a></li>
+       <li><a href ="Service">Services</a></li>
+     </ul>
+    </div>
+   
+
+     
+  );
+}
+
+export default App;
